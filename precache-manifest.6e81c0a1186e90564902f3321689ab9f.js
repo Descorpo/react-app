5b5967830b14a4c3566f45f2d48d797461569bd1
Descorpo <Your new email>
2019-03-20 23:41:15 +0200

    Updates

---
+++ precache-manifest.6e81c0a1186e90564902f3321689ab9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.61045775.js"
+  },
+  {
+    "revision": "4e4ba8d4e6f2baa939bb",
+    "url": "/react-app/static/js/main.963e5e9b.chunk.js"
+  },
+  {
+    "revision": "b2f01e5c724eb6fbe143",
+    "url": "/react-app/static/js/2.f8916f28.chunk.js"
+  },
+  {
+    "revision": "4e4ba8d4e6f2baa939bb",
+    "url": "/react-app/static/css/main.9585944b.chunk.css"
+  },
+  {
+    "revision": "57d5ac7db754964ddf62747272de4ed3",
+    "url": "/react-app/index.html"
+  }
+];
